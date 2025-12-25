f08681e784d570bfd578f9c7bd77a5332053f031
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2025-12-24 22:33:07 -0300

    df

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,18 @@
 class Story < ApplicationRecord
 belongs_to :mytype
 has_and_belongs_to_many :storypeople, :join_table => :storyhaspeople
+def content=(uploaded_io)
+if self.mytype.name == "video" or self.mytype.name == "photo"
+File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+  file.write(uploaded_io.read)
+end
+write_attribute(:content, uploaded_io.original_filename)
+else
+write_attribute(:content, uploaded_io)
+end
+end
+def content
+read_attribute(:mypic)
+end
+
 end
